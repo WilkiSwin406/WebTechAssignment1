8806fd98b5b16d84b45ba87be710f83cb3f148a3
Darcy368 <105920569@student.swin.edu.au>
2025-05-14 11:47:51 +1000

    addded manage.php

---
+++ project1/manage.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="the managment for the IT company CyberBytes" />
+  <meta name="keywords" content="CyberBytes, managment, info tech , web design" />
+  <meta name="author" content="Swinburne Sigmas"  />
+  <title>CyberBytes managment</title>
+  
+  <link rel="stylesheet" href="../styles/styles.css">
+</head>
