d13c024b05b8cdd76964e2f90023861b3cc2fc91
Darcy368 <105920569@student.swin.edu.au>
2025-05-17 15:42:45 +1000

    removed files replacing them with switch statment in EOI_listing

---
+++ project1/manage/EOI_listing.php
@@ -0,0 +1,12 @@
+<?php
+    EOI_Query = 1
+    switch eoi
+    {
+        case 1 #list all EOIs
+            
+        case 2 #list EOIs for position
+    
+        case 3 #list EOIs for indiviual
+
+    }
+?>
