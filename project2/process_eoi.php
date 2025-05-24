22e4edff756c7a44b0cae0b040876f79e9d2e1ae
Connor Wright <s3946903@student.rmit.edu.au>
2025-05-24 15:36:25 +1000

    prototype database integration with process_eoi.php - the page now interfaces with the database, adds new records to it, and returns the EOI ID to the user if successful. if the data fails to validate, it will not create a new record, and will provide an error message for each problem with the input.

--- project2/process_eoi.php
+++ project2/process_eoi.php
@@ -1,3 +1,13 @@
+<?php
+require_once('settings.php');
+$conn = mysqli_connect($host, $user, $pwd, $sql_db);
+
+if (!$conn){
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,102 +29,125 @@
             $addressState = clean_input($_POST["State"]);
             $phone = clean_input($_POST["Phone"]);
             $skills = skills_string();
+            $jrn = clean_input($_POST["Job"]);
+            $validationSuccessful = True;
 
-            
+            // TODO: REDIRECT DIRECT LINKS FROM PROCESS_EOI.PHP TO APPLY.PHP
 
             if (empty($fname) || empty($lname)) {
                 echo "Error: Your full name is required.<br>";
+                $validationSuccessful = False;
             }
 
             if (empty($email)) {
                 echo "Error: Your email is required.<br>";
+                $validationSuccessful = False;
             } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) { # this is a check to make sure that the email entered is formatted as an email should be, adding a second check to the pattern already contained within the form entry field
                 echo "Error: Invalid email format.<br>";
+                $validationSuccessful = False;
             }
 
             
 
             if (empty($dob)) {
                 echo "Error: Please enter your date of birth.<br>";
+                $validationSuccessful = False;
             }
             
             if (empty($addressStreet)) {
                 echo "Error: Please enter your street address.<br>";
+                $validationSuccessful = False;
             }
 
             if (empty($addressSuburb)) {
                 echo "Error: Please enter your suburb or town.<br>";
+                $validationSuccessful = False;
             }
 
             if (empty($addressPostcode)) {
                 echo "Error: Please enter your postcode.<br>";
+                $validationSuccessful = False;
             }
 
             if (empty($phone)) {
                 echo "Error: Please enter your phone number.<br>";
+                $validationSuccessful = False;
             }
 
             switch ($addressState) { // this switch case sanity checks the postcode to make sure it matches with the chosen state
                 case "ACT":
                     if (!(($addressPostcode <= 2618) && (2600 <= $addressPostcode) || ($addressPostcode <= 2920) && (2900 <= $addressPostcode)))  {
                         echo "Error: Your postcode $addressPostcode does not match your state, $addressState.<br>";
+                        $validationSuccessful = False;
                     }
                     break;
                 case "NSW":
                     if (!(($addressPostcode <= 2599) && (2000 <= $addressPostcode) || ($addressPostcode <= 2899) && (2619 <= $addressPostcode) || ($addressPostcode <= 2999) && (2621 <= $addressPostcode))) {
                         echo "Error: Your postcode $addressPostcode does not match your state, $addressState.<br>";
+                        $validationSuccessful = False;
                     }
                     break;
                 case "NT":
                     if (!(($addressPostcode <= 899) && (800 <= $addressPostcode))) {
                         echo "Error: Your postcode $addressPostcode does not match your state, $addressState.<br>";
+                        $validationSuccessful = False;
                     }
                     break;
                 case "QLD":
                     if (!(($addressPostcode <= 4999) && (4000 <= $addressPostcode)))  {
                         echo "Error: Your postcode $addressPostcode does not match your state, $addressState.<br>";
+                        $validationSuccessful = False;
                     }
                     break;
                 case "SA":
                     if (!(($addressPostcode <= 5799) && (5000 <= $addressPostcode)))  {
                         echo "Error: Your postcode $addressPostcode does not match your state, $addressState.<br>";
+                        $validationSuccessful = False;
                     }
                     break;
                 case "TAS":
                     if (!(($addressPostcode <= 7799) && (7000 <= $addressPostcode)))  {
                         echo "Error: Your postcode $addressPostcode does not match your state, $addressState.<br>";
+                        $validationSuccessful = False;
                     }
                     break;
                 case "VIC":
                     if (!(($addressPostcode <= 3996) && (3000 <= $addressPostcode))) {
                         echo "Error: Your postcode $addressPostcode does not match your state, $addressState.<br>";
+                        $validationSuccessful = False;
                     }
                     break;
                 case "WA":
                     if (!(($addressPostcode <= 6797) && (6000 <= $addressPostcode)))  {
                         echo "Error: Your postcode $addressPostcode does not match your state, $addressState.<br>";
+                        $validationSuccessful = False;
                     }
                     break;
             }
 
             if (!(ctype_alpha($fname))) { # this function and the one directly after use a ctype function to make sure an entered first and last name uses no numbers or special characters
                 echo "Error: Your first name must contain only alphabetical characters.<br>";
+                $validationSuccessful = False;
             }
 
             if (!(ctype_alpha($lname))) {
                 echo "Error: Your last name must contain only alphabetical characters.<br>";
+                $validationSuccessful = False;
             }
 
             if (strlen($phone) > 12 || strlen($phone) < 8) { # here we use strlen() to get the length of the entered phone number. This one is important because the patterns on the HTML side don't prevent the phone number from being the wrong length like they do with the applicant's names and street/suburb names
                 echo "Error: Your phone number should be between 8 and 12 characters long, including spaces.<br>";
+                $validationSuccessful = False;
             }
 
             if (!(only_numbers_or_spaces($phone))) {
                 echo "Error: Your phone number should contain only numbers and spaces.<br>";
+                $validationSuccessful = False;
             }
             
             if (empty($skills)) {
                 echo "Error: Please select at least one technical skill.<br>";
+                $validationSuccessful = False;
             }
 
             if (isset($_POST["Other-skills-checkbox"])) {
@@ -122,9 +155,11 @@
                     $otherSkills = clean_input($_POST["Other-skills"]);
                     if (empty($otherSkills)) {
                         echo "Error: 'Other Skills' checkbox is selected, but no text is entered in the relevant text box. Please either enter your skills into the text box, or uncheck the checkbox.<br>";
+                        $validationSuccessful = False;
                     }
                 } else {
                     echo "Error: 'Other Skills' checkbox is selected, but no text is entered in the relevant text box. Please either enter your skills into the text box, or uncheck the checkbox.<br>";
+                    $validationSuccessful = False;
                 }
             } else {
                 $otherSkills = "No other skills entered";
@@ -132,21 +167,35 @@
 
             }
 
+            # create a table / check if inputs validated
+            
+            if ($validationSuccessful) {
+                $sql = "INSERT INTO eoi (JRN, FirstName, LastName, Gender, DOB, StreetAddress, Suburb, State, Postcode, Email, Phone, Skills, OtherSkills)
+                        VALUES ('$jrn', '$fname', '$lname', '$gender', '$dob', '$addressStreet', '$addressSuburb', '$addressState', '$addressPostcode', '$email', '$phone', '$skills', '$otherSkills')";
+                if ($conn->query($sql) === TRUE) {
+                    $eoiID = $conn->insert_id;
+                    echo "Your expression of interest form has been submitted, with the ID " . $eoiID . ". Have a nice day!";
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
 
+            } else {
+                echo "Please click <a href='apply.php'>here</a> to return to the previous page.<br>";
+            }
 
-
-            echo "$fname<br>";
-            echo "$lname<br>";
-            echo "$email<br>";
-            echo "$dob<br>";
-            echo "$addressStreet<br>";
-            echo "$addressSuburb<br>";
-            echo "$addressPostcode<br>";
-            echo "$addressState<br>";
-            echo "$phone<br>";
-            echo "$gender<br>";
-            echo "$skills<br>";
-            echo "$otherSkills<br>";
+            // TODO: REMOVE THIS
+            // echo "$fname<br>";
+            // echo "$lname<br>";
+            // echo "$email<br>";
+            // echo "$dob<br>";
+            // echo "$addressStreet<br>";
+            // echo "$addressSuburb<br>";
+            // echo "$addressPostcode<br>";
+            // echo "$addressState<br>";
+            // echo "$phone<br>";
+            // echo "$gender<br>";
+            // echo "$skills<br>";
+            // echo "$otherSkills<br>";
 
 
 
--- swinburnesigmas_db_24.sql
+++ swinburnesigmas_db_24.sql
@@ -32,14 +32,16 @@ CREATE TABLE `eoi` (
   `JRN` varchar(6) NOT NULL,
   `FirstName` varchar(20) NOT NULL,
   `LastName` varchar(20) NOT NULL,
+  `Gender` set('MALE','FEMALE','NON-BINARY','OTHER','PREFER NOT TO SAY') NOT NULL,
+  `DOB` date NOT NULL,
   `StreetAddress` varchar(40) NOT NULL,
-  `Town/Suburb` varchar(40) NOT NULL,
+  `Suburb` varchar(40) NOT NULL,
   `State` set('VIC','ACT','TAS','QLD','NT','SA','WA','NSW') NOT NULL,
   `Postcode` int(4) NOT NULL,
   `Email` varchar(99) NOT NULL,
   `Phone` int(12) NOT NULL,
   `Skills` varchar(100) NOT NULL,
-  `OtherSkills` text DEFAULT NULL,
+  `OtherSkills` text DEFAULT NOT NULL,
   `Status` set('NEW','CURRENT','FINAL') NOT NULL DEFAULT 'NEW'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -67,7 +69,14 @@ INSERT INTO `jobs` (`id`, `JRN`, `Description`, `Salary_range`, `Position`, `Req
 (8, 'CLE01', 'As one of our cloud engineers, you will be responsible for designing, implementing and maintaining our cloud-based infrastructure and services. You must ensure that our cloud systems are scalable, secure and reliable at all times. Your position will primarily consist of maintenance work as a level one employee.', '$100,000 - $105,000', 'Cloud Engineer, Level 1', '2+ years of experience in cloud platforms (AWS, Azure, or GCP)\nStrong understanding of networking, security, and cloud architecture\nProficient in scripting languages (e.g., Python, Bash)', 'Experience with containerization (Docker, Kubernetes)\nKnowledge of cost optimization strategies in the cloud\nFamiliarity with Infrastructure as Code (IaC) tools such as Terraform'),
 (9, 'DBE01', 'As a database analyst, you will be responsible for creating efficient database systems, optimizing queries, and implementing backup and recovery strategies for our databases. Your position will also include handling scaling and integration tasks, as well as database migration if necessary. As a level one employee, you will primarily focus on maintenance.', '$105,000 - $115,000', 'Database Engineer, Level 1', '2+ years of experience in data analysis or related field\nProficiency in data visualization tools (e.g., Power BI, Tableau, Looker)\nUnderstanding of statistical analysis and data interpretation', 'Proficiency in Python or R for data analysis\nExperience with big data tools (e.g., Spark, Hadoop)'),
 (10, 'DTA02', 'As a data analyst, you will be responsible for collecting, processing and analysing data in order to assist management to make informed decisions. You will identify trends, patterns and insights through data visualisation and statistical techniques. As a level two employee, you will also be expected to prepare reports for stakeholders and management, as well as participate in meetings in order to share your findings.', '$110,000 - $135,000', 'Data Analyst, Level 2', '4+ years of experience in database development and administration\nProficiency in SQL and database design principles\nExperience with at least one RDBMS (e.g., PostgreSQL, MySQL, SQL Server)', 'Experience with NoSQL databases (e.g., MongoDB, Cassandra)\nCertification in database technologies'),
-(11, 'UXD01', 'As one of our UX designers, you will be responsible for the user experience, including tasks such as upkeep of our website and app\'s user interface and designing prototypes or wireframes. You must ensure that your work is functional, readable and aesthetically pleasing. As a level one employee, you will primarily undertake smaller UI design tasks directly under your supervisor.', '$90,000 - $105,000', 'UX Designer, Level 1', '2+ years of experience in UX design\nStrong portfolio demonstrating UX processes and final designs\nKnowledge of responsive and accessible design principles', 'Experience working in Agile or cross-functional teams\nFamiliarity with HTML/CSS and design handoff to developers');
+(11, 'UXD01', 'As one of our UX designers, you will be responsible for the user experience, including tasks such as upkeep of our website and apps user interface and designing prototypes or wireframes. You must ensure that your work is functional, readable and aesthetically pleasing. As a level one employee, you will primarily undertake smaller UI design tasks directly under your supervisor.', '$90,000 - $105,000', 'UX Designer, Level 1', '2+ years of experience in UX design\nStrong portfolio demonstrating UX processes and final designs\nKnowledge of responsive and accessible design principles', 'Experience working in Agile or cross-functional teams\nFamiliarity with HTML/CSS and design handoff to developers');
+
+--
+-- Dumping data for table `eoi`
+--
+
+INSERT INTO `eoi` (`EOINumber`, `JRN`, `FirstName`, `LastName`, `Gender`, `DOB`, `StreetAddress`, `Suburb`, `State`, `Postcode`, `Email`, `Phone`, `Skills`, `OtherSkills`, `Status`) VALUES
+(7, 'DTA02', 'Connor', 'Wright', 'MALE', '2004-01-21', '26 Riviera St', 'Mentone', 'VIC', 3194, 'connorawright999@gmail.com', 416963332, ' Digital Networking Knowledge, Virtualisation, Automation Tools, UI & UX Understanding', '3-time hotdog eating champion', 'NEW');
 
 --
 -- Indexes for dumped tables
